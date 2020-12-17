d953b9f36f1de380c3148f3d099aa164deab79ba
Maurice <Maurice@192.168.0.11>
2020-12-17 18:15:13 +0100

    Item Frames are now protected when not in buildmode.

--- RediCraft_LS_1_15_2/src/at/mlps/rc/event/Blocker.java
+++ RediCraft_LS_1_15_2/src/at/mlps/rc/event/Blocker.java
@@ -1,17 +1,22 @@
 package at.mlps.rc.event;
 
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Fish;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.entity.EntityDamageEvent.DamageCause;
 import org.bukkit.event.entity.FoodLevelChangeEvent;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.player.PlayerCommandPreprocessEvent;
 import org.bukkit.event.player.PlayerDropItemEvent;
+import org.bukkit.event.player.PlayerInteractEntityEvent;
 import org.bukkit.event.weather.WeatherChangeEvent;
 
+import at.mlps.rc.cmd.BuildClass;
 import at.mlps.rc.main.LanguageHandler;
 import at.mlps.rc.main.Main;
 
@@ -41,6 +46,32 @@ public class Blocker implements Listener{
 	}
 	
 	@EventHandler
+	public void onItemFrameInteract(PlayerInteractEntityEvent e) {
+		Player p = e.getPlayer();
+		Entity target = e.getRightClicked();
+		if(target.getType() == EntityType.ITEM_FRAME) {
+			if(BuildClass.build.contains(p.getName())) {
+				e.setCancelled(false);
+			}else {
+				e.setCancelled(true);
+			}
+		}
+	}
+	
+	@EventHandler
+	public void onItemFrameDamage(EntityDamageByEntityEvent e) {
+		Entity issuer = e.getDamager();
+		Entity damaged = e.getEntity();
+		if(damaged.getType() == EntityType.ITEM_FRAME) {
+			e.setCancelled(true);
+			if(issuer.getType() == EntityType.PLAYER) {
+				Player p = (Player) issuer;
+				p.sendMessage(Main.prefix() + "§7You can't do that!");
+			}
+		}
+	}
+	
+	@EventHandler
 	public void onFood(FoodLevelChangeEvent e) {
 		e.setCancelled(true);
 	}
@@ -64,8 +95,6 @@ public class Blocker implements Listener{
         }else if (e.getCurrentItem().getItemMeta().getDisplayName().equals(LanguageInv.mainTitle)) {
         	e.setCancelled(true);
             p.sendMessage(nomove);
-        }else {
-        	e.setCancelled(true);
         }
     }
 	
